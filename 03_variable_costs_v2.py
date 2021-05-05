1cf483ca7acd860be1c843c4db0a33f30c63e85c
Iszac <58008716+jarvisi71729@users.noreply.github.com>
2021-05-05 14:25:05 +1200

    1 and a half

--- 03_variable_costs_v2.py
+++ 03_variable_costs_v2.py
@@ -43,40 +43,45 @@ def get_expenses(var_fixed):
     quantity_list = []
     price_list = []
 
-    variable_dict
+    variable_dict = {
+        "Item": item_list,
+        "Quantity": quantity_list,
+        "Price": price_list
+    }
+
+    item_name = ""
+    while item_name.lower() != "xxx":
+
+        print()
+        # get name, quantity and item
+        item_name = not_blank("Item name: ", "The compnent name cannot be blank.")
+
+        if item_name.lower() == "xxx":
+            break
+
+        quantity = num_check("Quantity: ", "The amount must be a whole number more than zero", int)
+
+        price = num_check("How much for a single item? $", "The price must be a number <more than 0>", float)
+
+        # add item, quantity and price to lists
+        item_list.append(item_name)
+        quantity_list.append(quantity)
+        price_list.append(price)
+
+    expense_frame = pandas.DataFrame(variable_dict)
+    expense_frame = expense_frame.set_index('Item')
+
+    # Calculate cost of each component
 
 
 
-# main routine starts here
-
-# set up dictionaries
-item_list = []
-quantity_list = []
-price_list = []
-
-variable_dict = {
-    "Item": item_list,
-    "Quantity": quantity_list,
-    "Price": price_list
-}
-
-# get user data
-product_name = not_blank("Product name: ", "The product name cannot be blank")
+# # main routine starts here
+#
+#
+# # get user data
+# product_name = not_blank("Product name: ", "The product name cannot be blank")
 
 # loop to get component, quantity and price
-item_name = ""
-while item_name.lower() != "xxx":
-
-    print()
-    # get name, quantity and item
-    item_name = not_blank("Item name: ", "The compnent name cannot be blank.")
-
-    if item_name.lower() == "xxx":
-        break
-
-    quantity = num_check("Quantity: ", "The amount must be a whole number more than zero", int)
-
-    price = num_check("How much for a single item? $", "The price must be a number <more than 0>", float)
 
     # add item, quantity and price to list
     item_list.append(item_name)
