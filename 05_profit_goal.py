1e90cff86d4ef8ac486c984a351ede2d796934b0
Iszac <58008716+jarvisi71729@users.noreply.github.com>
2021-05-14 11:48:32 +1200

    profit goal

---
+++ 05_profit_goal.py
@@ -0,0 +1,94 @@
+# functions go here
+
+# checks that the input is yes or no otherwise prints an error
+def yes_no(question):
+
+    error = "please answer yes/no"
+
+    valid = False
+    while not valid:
+
+        # ask question and put response in lowercase
+        response = input(question).lower()
+        if response == "yes" or response == "y":
+            return response
+        elif response == "no" or response == "n":
+            return response
+        else:
+            print(error)
+
+def profit_goal(total_costs):
+
+    # Initialise variables and error message
+    error = "Please enter a valid profit goal\n"
+
+    valid = False
+    while not valid:
+
+        # ask for profit goal...
+        response = input("What is your profit goal? (eg $500 or 50%) ")
+
+        # check if first character is $...
+        if response[0] == "$":
+            profit_type = "$"
+            # get amount (everything after the $)
+            amount = response[1:]
+
+        # check if last character is %
+        elif response [-1] == "%":
+            profit_type = "%"
+            # get amount (everything before the %)
+            amount = response[:-1]
+
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # check amount is a number more than zero
+            amount = float(amount)
+            if amount <= 0:
+                print(error)
+                continue
+
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unknown" and amount >= 100:
+            dollar_type = yes_no("Do you mean ${:.2f}.  "
+                                 "ie {:.2f} dollars?, "
+                                 "y / n ".format(amount, amount))
+
+            # set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif profit_type == "unknown" and amount < 100:
+            percent_type = yes_no("Do you mean {}%? , "
+                                  "y / n ".format(amount))
+            if percent_type == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        # return profit goal to main routine
+        if profit_type == "$":
+            return amount
+        else:
+            goal = (amount / 100) * total_costs
+            return goal
+
+
+# Main Routine
+all_costs = 200
+
+# Loop for quick testing...
+for item in range(0, 6):
+    profit_target = profit_goal(all_costs)
+    print("Profit Target: ${:.2f}".format(profit_target))
+    print("Total Sales: ${:.2f}".format(all_costs + profit_target))
+    print()
